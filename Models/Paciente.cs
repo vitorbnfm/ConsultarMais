2e69739742a10375cc1a5ee88342fe3fa127739a
Vitor Bonfim <50812235+vitorbnfm@users.noreply.github.com>
2021-09-27 15:31:42 -0300

    Paciente.cs atualizado

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -12,7 +12,7 @@ namespace Consultar.Models
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
         public string Endereco { get; set; }
-        public int Celular { get; set; }
+        public string Celular { get; set; }
         public string Email { get; set; }
 
         public DateTime CriadoEm { get; set; }
