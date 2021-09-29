6c900825cc93812ec81e671571bc63a9ca92f9a1
Igor <igor.m99@hotmail.com>
2021-09-29 17:35:22 -0300

    Fazendo o login e construindo novos métodos para o usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -27,9 +27,19 @@ namespace Consultar.Controllers
 
         public IActionResult Create([FromBody] Usuario usuario)
         {
-            _context.Usuarios.Add(usuario);
-            _context.SaveChanges();
-            return Created("", usuario);
+
+
+            if (usuario.Login.Length < 8 || usuario.Senha.Length < 8)
+            {
+                return BadRequest("Login e senha devem conter no mínimo 8 caracteres");
+            }
+            else
+            {
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
+                return Created("", usuario);
+            }
+
         }
 
         //GET api/usuario/listar
@@ -37,5 +47,71 @@ namespace Consultar.Controllers
         [Route("listar")]
 
         public IActionResult List() => Ok(_context.Usuarios.ToList());
+
+        [HttpGet]
+        [Route("getbyid/{id}")]
+
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Usuario usuario = _context.Usuarios.Find(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(usuario);
+            }
+        }
+
+        //DELETE: api/usuario/delete/
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete([FromRoute] string name)
+        {
+            Usuario usuario = _context.Usuarios.FirstOrDefault
+            (
+                usuario => usuario.Nome == name
+            );
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+            return Ok(_context.Usuarios.ToList());
+        }
+
+        //PUT: api/usuario/update
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            _context.SaveChanges();
+            return Ok(usuario);
+        }
+
+        //GET: api/usuario/login
+        [HttpGet]
+        [Route("login")]
+
+        public IActionResult Login(string login, string senha)
+        {
+            Usuario usuario = _context.Usuarios.FirstOrDefault(
+                u => u.Login == login && u.Senha == senha
+            );
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(usuario);
+            }
+        }
+
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -4,12 +4,15 @@ namespace Consultar.Models
 {
     public class Usuario
     {
-
-
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Login { get; set; }
         public string Senha { get; set; }
+
+        public static bool ProcuraLogin(string login)
+        {
+            return false; // falta terminar
+        }
 
         public override string ToString() =>
             $"Nome: {Nome} | Login: {Login} | Senha: {Senha}";
